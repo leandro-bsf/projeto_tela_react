437f0f04280f728529eb1dc066c07b323be2a034
leandro barbosa <motorola1710@gmail.com>
2024-10-22 08:03:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import GraficoIncorporado from './components/GraficoIncorporado/GraficoIncorporado'; // Para incorporar o gráfico do Looker Studio
-// import GraficoComChartJS from './GraficoComChartJS'; // Para o gráfico com Chart.js
+import { WebView } from 'react-native-webview';
+import { StyleSheet, View, Text } from 'react-native';
 
 const App = () => {
-    return (
-        <div>
-            {/* Descomente a linha abaixo para usar o gráfico do Looker Studio */}
-            <GraficoIncorporado />
-            {/* Descomente a linha abaixo para usar o gráfico com Chart.js */}
-            {/* <GraficoComChartJS /> */}
-        </div>
-    );
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Gráfico do Looker Studio</Text>
+      <WebView
+        source={{ uri: 'https://lookerstudio.google.com/embed/reporting/2c2767a8-6448-4fe2-b176-7fc6775b82b3/page/KSiDE' }}
+        style={styles.webview}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  webview: {
+    height: 600,
+    width: '100%',
+  },
+});
 
 export default App;
